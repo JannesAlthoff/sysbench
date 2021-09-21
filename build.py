5b68180473c1c17165840a87d42c9640dbfebafc
Vitaly Potyarkin <sio.wtf@gmail.com>
2021-09-21 14:07:54 +0300

    Use bootstrap for output styling

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+import os
 import yaml
 import jinja2
 
@@ -5,6 +6,11 @@ DB_FILE = 'database.yml'
 FIELDS_FILE = 'fields.yml'
 TEMPLATE_DIR = 'templates/'
 OUTPUT_FILE = 'index.html'
+
+BOOTSTRAP_DEFAULT = {
+    'css': 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css',
+    'js': 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js',
+}
 
 def main():
     '''Build static HTML page from YAML data and Jinja2 templates'''
@@ -16,9 +22,19 @@ def main():
     jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR))
     template = jinja_env.get_template('index.html.j2')
 
+    bootstrap = BOOTSTRAP_DEFAULT
+    for component in bootstrap:
+        override = os.environ.get(f'BOOTSTRAP_{component.upper()}')
+        if override:
+            bootstrap[component] = override
+
     with open(OUTPUT_FILE, 'w') as output:
         output.write(
-            template.render(fields=fields, data=data)
+            template.render(
+                bootstrap=bootstrap,
+                data=data,
+                fields=fields,
+            )
         )
 
 if __name__ == '__main__':
